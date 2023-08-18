2c6bdbe1823d9967ea03f8d9bf69712450de2e34
nicka <nickallee8529@gmail.com>
2023-08-17 22:29:19 -0500

    added tests for firefox and chrome. added logging info on extent report

--- src/main/java/nicholas/allee/test/automation/framework/pageEvents/LoginPageEvents.java
+++ src/main/java/nicholas/allee/test/automation/framework/pageEvents/LoginPageEvents.java
@@ -2,20 +2,23 @@ package nicholas.allee.test.automation.framework.pageEvents;
 
 import nicholas.allee.test.automation.framework.pageObjects.HomePageElements;
 import nicholas.allee.test.automation.framework.pageObjects.LoginPageElements;
+import nicholas.allee.test.automation.framework.testBase.TestBase;
 import nicholas.allee.test.automation.framework.utilities.ElementFetch;
 import org.testng.Assert;
 
-public class LoginPageEvents {
+public class LoginPageEvents{
     ElementFetch element  = new ElementFetch();
     public void verifyIfLoginPageIsLoaded(){
         Assert.assertFalse(element.getWebElements("XPATH", LoginPageElements.usernameField).isEmpty(), "unable to locate username element inside login page.");
     }
     public void enterUsernamePassword(String username, String password){
+        //logger.info("Inserting username and password into login screen.");
         element.getWebElement("XPATH", LoginPageElements.usernameField).sendKeys(username);
         element.getWebElement("XPATH", LoginPageElements.passwordField).sendKeys(password);
     }
 
     public void clickSignInButton() throws InterruptedException {
+        //logger.info("Clicking Login Button.");
         element.getWebElement("XPATH", LoginPageElements.loginButtonField).click();
     }
 
--- src/test/java/QATestOrangeHRM.java
+++ src/test/java/QATestOrangeHRM.java
@@ -6,8 +6,11 @@ public class QATestOrangeHRM extends TestBase {
 
     @Test
     public void testLoginCredentials() throws InterruptedException {
+        logger.info("Verifying that the loading page is loaded.");
         pageEventHolder.loginPageEvents.verifyIfLoginPageIsLoaded();
+        logger.info("Entering username and password.");
         pageEventHolder.loginPageEvents.enterUsernamePassword(customParams.username,customParams.password);
+        logger.info("Clicking Signing Button.");
         pageEventHolder.loginPageEvents.clickSignInButton();
         Thread.sleep(1000L);
     }
